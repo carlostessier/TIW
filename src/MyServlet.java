8b5751009c1c21ddd6cc7ac68b58e7515acfd1f0
Tecnologias de Internet para la web <tiw@vagrant.vm>
2016-09-15 18:40:51 +0000

    exercise 1

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -1,55 +1,77 @@
 
-
 import java.io.*;
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.RequestDispatcher;
 
-import java.util.Enumeration;
 
 /**
  * Servlet implementation class MyServlet
  */
+
 public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MyServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MyServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		
+		
+
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Read the parameters from the request
+				String sName = request.getParameter("Name");
+				String sEmail = request.getParameter("Email");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	
+				// Set the content type
+				response.setContentType("text/html");
+				// Get the writer
+				PrintWriter out = response.getWriter();
+				// Write the output page
+				out.println("<!DOCTYPE html>");
+				out.println("<html>");
+				out.println("<head>");
+				out.println("<title>Form Fields</title>");
+				out.println("</head>");
+				out.println("<body>");
 
-			   
+				out.println("Client information ");
+				out.println("<h2>Name: " + sName + "</h2>");
+				out.println("<h2>Email: " + sEmail + "</h2>");
+				out.println("</body>");
+				out.println("</html>");
+				out.flush();
+				out.close(); // Inform the server that we finished
+				// sending the information
+
 	}
 
 }
